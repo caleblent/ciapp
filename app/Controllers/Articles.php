936e29379c2afa78b081b12ee97cccbcffd870a9
caleblent <caleb@stcroixwebagency.com>
2024-11-04 21:19:20 -0600

    made database/table migrations, Articles page now pulls dynamic data from database

--- app/Controllers/Articles.php
+++ app/Controllers/Articles.php
@@ -2,14 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\ArticleModel;
+
 class Articles extends BaseController
 {
     public function index() 
     {
-        $data = [
-            ["title" => "One", "content" => "The first piece"],
-            ["title" => "Two", "content" => "More content here"]
-        ];
+        // $db = db_connect();
+
+        // $db->listTables();
+
+        // $data = [
+        //     ["title" => "One", "content" => "The first piece"],
+        //     ["title" => "Two", "content" => "More content here"]
+        // ];
+
+        $model = new ArticleModel;
+
+        $data = $model->findAll();
 
         return view("Articles/index", [
             "articles" => $data
